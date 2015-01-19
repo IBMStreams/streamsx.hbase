45fe76e9c59adfb969d543a99bedafbf44c601fb
KIRSTEN W. HILDRUM <hildrum@us.ibm.com>
2015-01-19 12:09:21 -0500

    Move native functions from toolkit to sample

--- samples/CustomKey/impl/java/src/com/ibm/streamsx/hbase/sample/HelperFunctions.java
+++ samples/CustomKey/impl/java/src/com/ibm/streamsx/hbase/sample/HelperFunctions.java
@@ -1,4 +1,4 @@
-package com.ibm.streamsx.hbase;
+package com.ibm.streamsx.hbase.sample;
 
 import java.nio.ByteBuffer;
 import java.util.logging.Level;
@@ -8,20 +8,22 @@ import java.util.logging.Logger;
 
 
 import com.ibm.streams.function.model.Function;
-
 public class HelperFunctions {
-	public static final String NAMESPACE="com.ibm.streamsx.hbase";
+	public static final String NAMESPACE="com.ibm.streamsx.hbase.sample";
 	public static final Logger trace = Logger.getLogger(HelperFunctions.class.getCanonicalName());
 	//= LogManager.getLogger(HelperFunctions.class.getCanonicalName());
 	
-	@Function(namespace=NAMESPACE)
+	// The sample application uses the makeKey function to make a key,
+	// but an application could use a combination of the appendLong and appendString
+	// functions instead. 
+	@Function(namespace=NAMESPACE,description="Append the long, in bytes, to the current blob and return the resulting blob.  The original blob is unchanged.")
 	public static byte[] appendLong(byte[] currentBlob,long toAppend ) {
 		ByteBuffer buffer = ByteBuffer.allocate(currentBlob.length + Long.SIZE/Byte.SIZE);
 		buffer.put(currentBlob);
 		buffer.putLong(toAppend);
 		return buffer.array();
 	}
-	@Function
+	@Function(namespace=NAMESPACE,description="Get the type representation of the string toAppend (using the default character set), and append it to the blob, and return the result.  The original blob is unchanged.")
 	public static byte[] appendString(byte[] currentBlob, String toAppend) {
 		byte[] stringBytes = toAppend.getBytes();
 		byte[] toReturn = new byte[stringBytes.length+ currentBlob.length];
@@ -33,12 +35,12 @@ public class HelperFunctions {
 		}
 		return toReturn;
 	}
-	@Function
+	@Function(namespace=NAMESPACE,description="Starting at index, return the long represented by bytes in the blob from index to index+7")
 	public static long getLong(byte[] currentBlob,int index) {
 		ByteBuffer buffer = ByteBuffer.wrap(currentBlob);
 		return buffer.getLong(index);
 	}
-	@Function
+	@Function(namespace=NAMESPACE,description="Starting at index, return the string represented by the bytes index to index+length-1 in the blob.  Assumes the default character set.")
 	public static String getString(byte[] currentBlob, int offset, int length) {
 		trace.log(Level.INFO,"Entering getString, currentBlob size {0}, index {1}, length {2}",new Object[] {currentBlob.length,offset,length});
 		byte [] stringBytes = new byte[length];
